d1eec526be46a4bfe7604512aab5215f85d51049
berezinmv <berezin.mvdev@yahoo.com>
2016-06-29 19:52:51 +0500

    Add WebRTC typings

---
+++ src/client/utils.ts
@@ -0,0 +1,3 @@
+export const PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+export const SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+export const IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;

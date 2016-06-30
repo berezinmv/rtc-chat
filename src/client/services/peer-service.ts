eed784f7f74ce30a79e234b9a63ea1133689b6ff
berezinmv <berezin.mvdev@yahoo.com>
2016-06-30 11:59:45 +0500

    PeerService implemented

--- src/client/services/peer-service.ts
+++ src/client/services/peer-service.ts
@@ -1,3 +1,25 @@
-export class PeerService {
+import {WsService} from "./ws-service";
+import {UserStore} from "../stores/user-store";
 
+export class PeerService {
+  private static sendMessage(type: string, data: any, user: any) {
+    WsService.getClient().emit("webrtc", JSON.stringify({
+      user: UserStore.getInstance().getUser(),
+      receiver: user,
+      data: data,
+      type: type
+    }));
+  }
+
+  static sendCandidateMessage(candidate: RTCIceCandidate, user: any) {
+    PeerService.sendMessage("candidate", candidate, user);
+  }
+
+  static sendOfferMessage(description: RTCSessionDescription, user: any) {
+    PeerService.sendMessage("offer", description, user);
+  }
+
+  static sendAnswerMessage(description: RTCSessionDescription, user: any) {
+    PeerService.sendMessage("answer", description, user);
+  }
 }

37193b4900cbe2dcca2f95c5f0b5fb3a142d2c0c
berezinmv <berezin.mvdev@yahoo.com>
2016-06-30 11:59:59 +0500

    Fix typings errors

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,3 +1,3 @@
-export const PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-export const SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-export const IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+export const PeerConnection: {new(...args: Array<any>): RTCPeerConnection} = (window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection) as any;
+export const SessionDescription: {new(...args: Array<any>): RTCSessionDescription} = (window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription) as any;
+export const IceCandidate: {new(...args: Array<any>): RTCIceCandidate} = (window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate) as any;
